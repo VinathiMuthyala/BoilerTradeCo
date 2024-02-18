67b32fcea0c50e79fd1b8da0441cdc40a05784ce
isha-singhal1 <isingha@purdue.edu>
2024-02-18 16:19:13 -0500

    templates prototype

--- prototypes/Isha/playground/views.py
+++ prototypes/Isha/playground/views.py
@@ -7,4 +7,5 @@ from django.http import HttpResponse
 # action
 
 def say_hello(request):
-    return HttpResponse('Hello World')
+    #return HttpResponse('Hello World')
+    return render(request, 'hello.html', { 'name': 'Mosh'})
