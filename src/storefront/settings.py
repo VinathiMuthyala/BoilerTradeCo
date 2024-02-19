1c37b98bc1a87587ff7e97fe4bde23ec11159c1f
Puja Teakulapalli <57300087+pujareddyt2004@users.noreply.github.com>
2024-02-18 19:10:52 -0500

    basic html for homepage and signup page

--- src/storefront/settings.py
+++ src/storefront/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'storefront.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
