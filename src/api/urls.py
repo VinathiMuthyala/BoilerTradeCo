c7ae6a88411696d80450fd6955aa588f1585f008
nrajamani <nrajaman@purdue.edu>
2024-02-26 18:25:40 -0500

    got rid of API app

--- src/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ItemList, ItemDetail
-
-urlpatterns = [
-    path('items/', ItemList.as_view(), name='item-list'),
-    path('items/<int:pk>/', ItemDetail.as_view(), name='item-detail'),
-]
