6215688bf4a640456a99c66368fd7e8b79571ed6
nrajamani <nrajaman@purdue.edu>
2024-02-26 19:37:39 -0500

    adding to settings to accommodate for database installation

--- storefront/settings.py
+++ storefront/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 #import django_heroku
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,6 +129,8 @@ STATIC_FILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 # Configure Django App for Heroku
-import django_on_heroku
-django_on_heroku.settings(locals())
+#import django_on_heroku
+#django_on_heroku.settings(locals())
