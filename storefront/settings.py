9e466186dc241f677763ae20a93ea5896771563b
nrajamani <nrajaman@purdue.edu>
2024-02-28 17:22:09 -0500

    quick comment

--- storefront/settings.py
+++ storefront/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
-import django_heroku
+#import django_heroku
 from pathlib import Path
 import dj_database_url
 
