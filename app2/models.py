76264b18b9ece9e2065b70a97a6658d1aeb6852d
Vinathi Muthyala <vinathi.muthyala@gmail.com>
2024-03-01 13:03:37 -0500

    add product backend- work in progress

--- app2/models.py
+++ app2/models.py
@@ -1,3 +1,157 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+
+class QualityTag(models.Model):
+  tag = models.CharField(max_length=100, choices = [
+       ('Like New', 'Like New'),
+       ('Good', 'Good'),
+       ('Acceptable', 'Acceptable'),
+   ])
+
+   # get_quality()
+  def __str__(self):
+       return self.tag
+  
+   # will connect this in front end later
+  def set_quality(self, quality):
+       self.tag = quality
+
+
+class CategoryTag(models.Model):
+   tag= models.CharField(max_length=100, choices = [
+       ('Furniture', 'Furniture'),
+       ('Appliances', 'Appliances'),
+       ('Textbooks', 'Textbooks'),
+       ('School supplies', 'School supplies'),
+       ('Clothing', 'Clothing'),
+       ('Game tickets', 'Game tickets'),
+   ])
+   # get_category()
+   def __str__(self):
+       return self.tag
+
+class ProductInfo(models.Model):
+   seller_email = models.CharField(max_length=100, primary_key=True, unique=True)
+   name = models.CharField(max_length=100)
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   description = models.TextField()
+   quality_tag = models.ForeignKey(QualityTag, on_delete=models.SET_NULL, null=True, blank=True)
+   category_tag = models.ForeignKey(CategoryTag, on_delete=models.SET_NULL, null=True, blank=True)
+   date_posted = models.TextField(default=0000-00-00)
+   #seller_name = models.CharField(max_length=100)
+   #seller_phone_number = models.IntegerField(validators=[MaxValueValidator(9999999999)])
+   #seller_instagram = models.CharField(max_length=30) # max length of valid instagram username
+   #is_favorited = models.BooleanField(default=False)
+
+   @classmethod
+   def create_product(cls, seller_email, name=None, price=None, description=None, quality_tag=None, category_tag=None, date_posted=None):
+       existing_product = cls.objects.filter(seller_email=seller_email).first()
+
+       if existing_product:
+                # Update the existing product
+                existing_product.name = name
+                existing_product.price = price
+                existing_product.description = description
+                existing_product.quality_tag, _ = QualityTag.objects.get_or_create(tag=quality_tag)
+                existing_product.category_tag, _ = CategoryTag.objects.get_or_create(tag=category_tag)
+                existing_product.date_posted = date_posted
+                existing_product.save()
+                return existing_product
+       else:
+                # Create a new product
+                quality_tag_instance, _ = QualityTag.objects.get_or_create(tag=quality_tag)
+                category_tag_instance, _ = CategoryTag.objects.get_or_create(tag=category_tag)
+
+                return cls.objects.create(
+                    seller_email=seller_email,
+                    name=name,
+                    price=price,
+                    description=description,
+                    quality_tag=quality_tag_instance,
+                    category_tag=category_tag_instance,
+                    date_posted=date_posted
+        )
+
+
+
+    #    return ProductInfo.create_product(name, price, seller_email, description, quality_tag, category_tag, date_posted)
+    #    quality_tag_instance, _ = QualityTag.objects.get_or_create(tag=quality_tag)
+    #    category_tag_instance, _ = CategoryTag.objects.get_or_create(tag=category_tag)
+
+    #    return cls.objects.create(
+    #         seller_email=seller_email,
+    #         name=name,
+    #         price=price,
+    #         description=description,
+    #         quality_tag=quality_tag_instance,
+    #         category_tag=category_tag_instance,
+    #         date_posted=date_posted
+    #     )
+    # return self.__class__.objects.create(
+    #         name=name,
+    #         price=price,
+    #         seller_email=seller_email,
+    #         description=description,
+    #         quality_tag=quality_tag,
+    #         category_tag=category_tag,
+    #         date_posted=date_posted
+    # )
+
+   # get_name()
+   def __str__(self):
+       return self.name
+  
+   def set_name(self, name):
+       self.name = name
+  
+   def get_price(self):
+       return self.price
+  
+   def set_price(self, price):
+       self.price = price
+
+   def get_description(self):
+       return self.description
+  
+   def set_description(self, description):
+       self.description = description
+  
+#    def get_seller_name(self):
+#        return self.seller_name
+  
+#    def set_seller_name(self, seller_name):
+#        self.seller_name = seller_name
+  
+   def get_seller_email(self):
+       return self.seller_email
+  
+   def set_seller_email(self, seller_email):
+       self.seller_email = seller_email
+  
+#    def get_seller_phone_number(self):
+#        return self.seller_phone_number
+  
+#    def set_seller_phone(self, seller_phone):
+#        self.seller_phone = seller_phone
+  
+#    def get_seller_instagram(self):
+#        return self.seller_instagram
+  
+#    def set_seller_instagram(self, seller_instagram):
+#        self.seller_instagram = seller_instagram
+  
+#    def get_is_favorited(self):
+#        return self.is_favorited
+  
+#    def set_is_favorited(self, is_favorited):
+#        self.is_favorited = is_favorited
+
+class ProductListing(models.Model):
+    # will delete the listing when the product is deleted
+    product = models.OneToOneField(ProductInfo, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Listing for Product: {self.product.name} - Seller: {self.product.seller_email}"
+        # return str(self.product)
--- app2/views.py
+++ app2/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
+from django.db import models
+from datetime import date
+from django.contrib import messages
+from .models import ProductInfo, ProductListing
 
 # Create your views here.
 def layout(request):
@@ -8,90 +12,135 @@ def layout(request):
 def add_product(request):
     return render(request, 'productdir/add-product.html')
 
+def add_listing(request):
+    print("hello is this working")
+    if request.method == "POST":
+        if 'product_info' in request.POST:
+            seller_email = request.POST.get('sellerEmail')
+            product_name = request.POST.get('productName')
+            product_price = request.POST.get('productPrice')
+            product_description = request.POST.get('productDescription')
+            quality_tag = request.POST.get('qualityTag')
+            category_tag = request.POST.get('categoryTag')
+            date_posted = date.today().strftime("%Y-%m-%d")
+            print(product_name)
+            print(product_price)
+            print(seller_email)
+            print(product_description)
+            print(quality_tag)
+            print(category_tag)
+            print("the date: ", date_posted)
+
+            myproduct = ProductInfo.create_product(
+                seller_email=seller_email,
+                name=product_name,
+                price=product_price,
+                description=product_description,
+                quality_tag=quality_tag,
+                category_tag=category_tag,
+                date_posted=date_posted
+            )
+            # myproduct = ProductInfo.create_product(product_name, product_price, seller_email, product_description, quality_tag, category_tag, date_posted)
+            #myproduct = ProductInfo(name=product_name, price=product_price, seller_email = seller_email, description=product_description, quality_tag=quality_tag, category_tag=category_tag, date_posted = date_posted)
+            # myproduct.save()
+
+            existing_product = get_object_or_404(ProductInfo, seller_email=seller_email)
+
+            myproductlisting = ProductListing.objects.filter(product=existing_product).first()
+
+            if not myproductlisting:
+                myproductlisting = ProductListing(product=existing_product)
+                myproductlisting.save()
+
+            # myproductlisting = ProductListing(product=myproduct)
+            # myproductlisting.save()
+            return redirect('home/')
+    return render(request, "authentication/home.html")
+
 # Backend code
 
-class Product():
-    def __init__(self, name, price, description, quality_tag, category_tag, seller_name, seller_email, seller_phone, seller_instagram, is_favorited = False):
-        self.name = name
-        self.price = price
-        self.description = description
-        self.quality_tag = QualityTag()
-        self.category_tag = CategoryTag()
-        self.seller_name = seller_name
-        self.seller_email = seller_email
-        self.seller_phone = seller_phone
-        self.seller_instagram = seller_instagram
-        self.is_favorited = is_favorited
-    def get_name(self):
-        return self.name
-    def set_name(self, name):
-        self.name = name
-    def get_price(self):
-        return self.price
-    def set_price(self, price):
-        self.price = price
-    def get_description(self):
-        return self.description
-    def set_description(self, description):
-        self.description = description
-    def get_seller_name(self):
-        return self.seller_name
-    def set_seller_name(self, seller_name):
-        self.seller_name = seller_name
-    def get_seller_email(self):
-        return self.seller_email
-    def set_seller_email(self, seller_email):
-        self.seller_email = seller_email
-    def get_seller_phone(self):
-        return self.seller_phone
-    def set_seller_phone(self, seller_phone):
-        self.seller_phone = seller_phone
-    def get_seller_instagram(self):
-        return self.seller_instagram
-    def set_seller_instagram(self, seller_instagram):
-        self.seller_instagram = seller_instagram
-    def get_is_favorited(self):
-        return self.is_favorited
-    def set_is_favorited(self, is_favorited):
-        self.is_favorited = is_favorited
+# class Product():
+#     def __init__(self, name, price, description, quality_tag, category_tag, seller_name, seller_email, seller_phone, seller_instagram, is_favorited = False):
+#         self.name = name
+#         self.price = price
+#         self.description = description
+#         self.quality_tag = QualityTag()
+#         self.category_tag = CategoryTag()
+#         self.seller_name = seller_name
+#         self.seller_email = seller_email
+#         self.seller_phone = seller_phone
+#         self.seller_instagram = seller_instagram
+#         self.is_favorited = is_favorited
+#     def get_name(self):
+#         return self.name
+#     def set_name(self, name):
+#         self.name = name
+#     def get_price(self):
+#         return self.price
+#     def set_price(self, price):
+#         self.price = price
+#     def get_description(self):
+#         return self.description
+#     def set_description(self, description):
+#         self.description = description
+#     def get_seller_name(self):
+#         return self.seller_name
+#     def set_seller_name(self, seller_name):
+#         self.seller_name = seller_name
+#     def get_seller_email(self):
+#         return self.seller_email
+#     def set_seller_email(self, seller_email):
+#         self.seller_email = seller_email
+#     def get_seller_phone(self):
+#         return self.seller_phone
+#     def set_seller_phone(self, seller_phone):
+#         self.seller_phone = seller_phone
+#     def get_seller_instagram(self):
+#         return self.seller_instagram
+#     def set_seller_instagram(self, seller_instagram):
+#         self.seller_instagram = seller_instagram
+#     def get_is_favorited(self):
+#         return self.is_favorited
+#     def set_is_favorited(self, is_favorited):
+#         self.is_favorited = is_favorited
     
-class QualityTag(Product):
-    productQuality = [
-        ('Like New'),
-        ('Good'),
-        ('Acceptable'),
-    ]
-    def get_quality(Product):
-        return Product.quality_tag
+# class QualityTag(Product):
+#     productQuality = [
+#         ('Like New'),
+#         ('Good'),
+#         ('Acceptable'),
+#     ]
+#     def get_quality(Product):
+#         return Product.quality_tag
     
-    # will connect this in front end later
-    def set_quality(self, quality):
-        self.quality_tag = quality
+#     # will connect this in front end later
+#     def set_quality(self, quality):
+#         self.quality_tag = quality
     
-class CategoryTag(Product):
-    productCategory = [
-        ('Furniture'),
-        ('Appliances'),
-        ('Textbooks'),
-        ('School supplies'),
-        ('Clothing'),
-        ('Game tickets'),
-    ]
-    def get_category(Product):
-        return Product.category
+# class CategoryTag(Product):
+#     productCategory = [
+#         ('Furniture'),
+#         ('Appliances'),
+#         ('Textbooks'),
+#         ('School supplies'),
+#         ('Clothing'),
+#         ('Game tickets'),
+#     ]
+#     def get_category(Product):
+#         return Product.category
     
-    def set_category(self, category):
-        self.category = category
+#     def set_category(self, category):
+#         self.category = category
 
-class ProductDirectory(Product):
-    products = [
+# class ProductDirectory(Product):
+#     products = [
 
-    ]
+#     ]
 
-    def get_product(products, Product):
-        for i in products:
-            if products[i].name == Product.get_name():
-                return products[i]
+#     def get_product(products, Product):
+#         for i in products:
+#             if products[i].name == Product.get_name():
+#                 return products[i]
             
-    def set_product(products, Product):
-        products[len(products)] = Product
+#     def set_product(products, Product):
+#         products[len(products)] = Product
