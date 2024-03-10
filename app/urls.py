9cae4e0868e2d8a90882d8d659a46ee5bc686330
Puja Teakulapalli <57300087+pujareddyt2004@users.noreply.github.com>
2024-03-10 16:25:15 -0400

    fixed urls

--- app/urls.py
+++ app/urls.py
@@ -7,8 +7,7 @@ from django.contrib.auth.views import LogoutView
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('', auth_views.PasswordResetCompleteView.as_view(template_name="authentication/reset_password_complete.html"), name="password_reset_complete"),
-    #path('', views.index, name ="index"),
+    path('', views.index, name ="index"),
     path('home/', views.home, name = "home"),
     path('signup/', views.signup, name = "signup"),
     path('signin/', views.signin, name = "signin"),
@@ -20,5 +19,5 @@ urlpatterns = [
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="authentication/reset_password.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="authentication/reset_password_sent.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="authentication/reset.html"), name="password_reset_confirm"),
-    #path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="authentication/reset_password_complete.html"), name="password_reset_complete"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="authentication/reset_password_complete.html"), name="password_reset_complete"),
 ]
