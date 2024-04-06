fc5b065107450bca9b91be94e65c968f86119739
isha-singhal1 <isingha@purdue.edu>
2024-04-06 12:45:08 -0400

    fixing viewing products UI

--- app2/migrations/0001_initial.py
+++ app2/migrations/0001_initial.py
@@ -18,7 +18,7 @@ class Migration(migrations.Migration):
             name='CategoryTag',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('tag', models.CharField(choices=[('Furniture', 'Furniture'), ('Appliances', 'Appliances'), ('Textbooks', 'Textbooks'), ('School supplies', 'School supplies'), ('Clothing', 'Clothing'), ('Game tickets', 'Game tickets')], max_length=100)),
+                ('tag', models.CharField(choices=[('Furniture', 'Furniture'), ('Appliances', 'Appliances'), ('Textbooks', 'Textbooks'), ('School supplies', 'School supplies'), ('Clothing', 'Clothing'), ('Game tickets', 'Game tickets'), ('Other', 'Other')], max_length=100)),
             ],
             options={
                 'verbose_name_plural': 'CategoryTag',
@@ -41,7 +41,7 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=100)),
                 ('price', models.DecimalField(decimal_places=2, max_digits=10)),
                 ('description', models.TextField()),
-                ('date_posted', models.DateTimeField(default=0)),
+                ('date_posted', models.DateTimeField()), #default=0
                 ('category_tag', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app2.categorytag')),
                 ('seller_email', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
                 ('quality_tag', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='app2.qualitytag')),
