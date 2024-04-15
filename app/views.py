87274647a4ce209f2df23f0713d558627befe7b3
Puja Teakulapalli <57300087+pujareddyt2004@users.noreply.github.com>
2024-04-14 20:11:12 -0400

    updated models related to seller ratings + created basic rating form

--- app/views.py
+++ app/views.py
@@ -364,7 +364,6 @@ def filter_products_by_price(request):
 
 
 
-
 # def filter_products_by_category(request, category_tag):
 #     filtered_products = ProductInfo.objects.filter(category_tag__tag=category_tag)
 
