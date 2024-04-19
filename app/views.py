fef5c870e827e4b77506c06fcd8198b36818a7c3
Puja Teakulapalli <57300087+pujareddyt2004@users.noreply.github.com>
2024-04-18 22:13:56 -0400

    displayed previous rating

--- app/views.py
+++ app/views.py
@@ -424,7 +424,8 @@ def filter_products_by_price(request):
 #     return render(request, 'rate_seller.html')
 
 def rate_seller(request, seller_email):
-    return render(request, 'authentication/rate_seller.html', {'seller_email': seller_email})
+    existing_rating = SellerRating.objects.filter(seller_email=seller_email, user=request.user).first()
+    return render(request, 'authentication/rate_seller.html', {'seller_email': seller_email, 'existing_rating': existing_rating})
 
 def submit_rating(request):
     if request.method == 'POST':
